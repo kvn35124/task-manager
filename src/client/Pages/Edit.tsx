62093e8c2132d5219e95eee5cb08b21788230d5b
Kevin Auer <kvn35124@uab.edu>
2020-05-28 17:27:57 -0500

    fixed function of edit page

--- src/client/Pages/Edit.tsx
+++ src/client/Pages/Edit.tsx
@@ -7,9 +7,10 @@ class Edit extends React.Component<IEditProps, IEditState>{
     constructor(props: IEditProps) {
         super(props);
         this.state = {
-            title: '',
-            date: new Date,
-            description: ''
+            id: 0,
+            task_name: '',
+            due_date: new Date,
+            task_description: ''
         }
     }
 
@@ -18,29 +19,28 @@ class Edit extends React.Component<IEditProps, IEditState>{
         try {
             let [results]: any = await json(`/api/tasks/${this.props.match.params.id}`)
             this.setState({
-                title: results.task_name,
-                description: results.task_description
+                id: results.id,
+                task_name: results.task_name,
+                task_description: results.task_description
             })
         } catch (error) {
             console.log(error);
         }
     }
 
-    onChange = date => {
-        this.setState({ date });
-        console.log(this.state.date);
+    onChange = due_date => {
+        this.setState({ due_date });
     }
 
     async handleSubmit() {
         event.preventDefault();
         let updatedTask = {
-            title: this.state.title,
-            date: this.state.date,
-            description: this.state.description
+            task_name: this.state.task_name,
+            due_date: this.state.due_date,
+            task_description: this.state.task_description
         }
         try {
             let results = await json(`/api/tasks/${this.props.match.params.id}`, 'PUT', updatedTask);
-            console.log(results);
             if (results) {
                 this.props.history.push('/');
             }
@@ -58,10 +58,10 @@ class Edit extends React.Component<IEditProps, IEditState>{
                     <form className="form-group border shadow border-dark rounded p-3">
                         <h1 className="text-center">Edit This Task</h1>
                         <label className="mt-2">Task:</label>
-                        <input type="text" className="form-control" value={this.state.title} onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ title: e.target.value })} />
-                        <Calendar className="mt-2" value={this.state.date} onChange={this.onChange} />
+                        <input type="text" className="form-control" value={this.state.task_name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ task_name: e.target.value })} />
+                        <Calendar className="mt-2" value={this.state.due_date} onChange={this.onChange} />
                         <label className="mt-2">Task Description:</label>
-                        <textarea className="form-control" name="" id="" cols={30} rows={10} value={this.state.description} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({ description: e.target.value })}></textarea>
+                        <textarea className="form-control" name="" id="" cols={30} rows={10} value={this.state.task_description} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({ task_description: e.target.value })}></textarea>
                         <div className="d-flex justify-content-around">
                             <button className="btn btn-primary mt-2" onClick={(e: React.MouseEvent<HTMLButtonElement>) => this.handleSubmit()}>Submit</button>
                             <Link to={'/'} className="btn btn-success mt-2">Back to Tasks</Link>
@@ -76,9 +76,10 @@ class Edit extends React.Component<IEditProps, IEditState>{
 
 interface IEditProps extends RouteComponentProps<{ id: string }> { };
 interface IEditState {
-    title: string;
-    date: Date;
-    description: string;
+    id: number;
+    task_name: string;
+    due_date: Date;
+    task_description: string;
 };
 
 
